31c389d1e906714d4a9ec876b34ac6aa072cfb21
TTtomer <tomer.henn@gmail.com>
2017-09-07 23:18:48 +0300

    Add files via upload

---
+++ cs tomer 1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            int num;
+            int num2;
+            Random b = new Random();
+            num = b.Next(0, 101);
+            num2 = b.Next(0, 101);
+            while (a)
+            {
+                
+                Console.Write("{0} + {1} = ", num, num2);
+                a = num + num2 != int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine(num + num2);
+
+        }
+    }
+}
